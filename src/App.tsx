d448436b15df560553fe7013625ebc4069f603ef
Muhammad Fajar <muhammadfajar191@gmail.com>
2022-09-30 05:55:15 +0700

    coming soon template

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div className="cross">
+                    <div className="line-1"></div>
+                    <div className="line-2"></div>
+                    <div className="line-3"></div>
+                </div>
+                <div className="contents">
+                    <h1>COMING SOON</h1>
+                    <div className="progress">
+                        <div className="loading"></div>
+                    </div>
+                    <h4>30%</h4>
+                    <h5>Website Under Cronstruction, On Progress</h5>
+                    <a href="#download">
+                        <span>Portfolio.pdf</span>
+                    </a>
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
